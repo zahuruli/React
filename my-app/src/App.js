c79db7e59ebd43c68494d926b8b3a33ec502665d
zahurul-islam <zahurul8259@gmail.com>
2023-02-17 17:53:28 +0600

    JSX & JS styling with css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
 
+import './index.css'
+
 const todo="Todo title";
 const date1='02/17/2023'
 const date=new Date();
@@ -7,16 +9,22 @@ const fullDate=date.getDate()
 const name1="zahurul ";
 const name2=" islam"
 
+const Pstyle={
+  backgroundColor:"purple",
+  color:"white",
+  fontSize:"2rem"
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello worlds</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, a?</p>
-      <h3>{todo}</h3>
+      <h1 style={{color:"red",backgroundColor:"black"}}>Hello worlds</h1>
+      <p style={Pstyle}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, a?</p>
+      <h3 style={{fontSize:"3rem"}}>{todo}</h3>
       <p>{date1}</p>
       <p>{fullDate}</p>
       <p>{now}</p>
-      <p>{name1 + name2}</p>
+      <p className="headingStyle">{name1 + name2}</p>
     </div>
   );
 }
